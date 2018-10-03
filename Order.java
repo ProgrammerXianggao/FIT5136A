22ec4f9b98d721dce1d51f1497527af0b755ab7d
GX <2529317137@qq.com>
2018-10-03 20:17:17 +1000

    1

--- Order.java
+++ Order.java
@@ -5,19 +5,25 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 public class Order
 {
     // instance variables - replace the example below with your own
     private int x;
     private Cart cart;
+    private Date orderTime;
     
-    /**
+    /**https://www.cnblogs.com/remember-forget/p/6098462.html datetime
      * Constructor for objects of class Order
      */
     public Order()
     {
         // initialise instance variables
+        orderTime = new Date();
         x = 0;
+        
     }
 
     /**
@@ -30,5 +36,16 @@ public class Order
     {
         // put your code here
         return x + y;
+    }
+    
+    public void displayTime ()
+    {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(df.format(new Date()));
+    }
+    
+    public Date getTime()
+    {
+        return orderTime;
     }
 }
