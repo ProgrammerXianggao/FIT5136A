f01babb91d5246272c33f0ab0458beb7dd1fd64a
Gx <2529317137@qq.com>
2018-10-06 23:31:08 +1000

    123

--- ManageProductController.java
+++ ManageProductController.java
@@ -1,10 +1,27 @@
  
 
+import java.util.Scanner;
+
+
 public class ManageProductController {
 
+	private static UserInterface ui;
+	public ManageProductController()
+	{
+//		ui = new UserInterface();
+	}
 	public static void addProduct()
 	{
+		String productInfo;
+		String[] productInfoList = new String[7];
+		System.out.println("Please input product information");
+		System.out.println("The format is id,productName,saleMethod,shelfLife,source,category,price");
+		Scanner input = new Scanner(System.in);
 		
+		productInfo = input.nextLine();
+		productInfoList = productInfo.split(",");
+		Product product = new Product(Integer.parseInt(productInfoList[0]),productInfoList[1],productInfoList[2],Integer.parseInt(productInfoList[3]),productInfoList[4],productInfoList[5],Double.parseDouble(productInfoList[6]));
+		ui.productList.add(product);
 	}
 	
 	public static void deleteProduct()
--- UserInterface.java
+++ UserInterface.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.*;
 import java.util.Scanner;
+
+
 import java.util.ArrayList;
 
 
@@ -17,32 +19,34 @@ import java.util.ArrayList;
  */
 public class UserInterface
 {
-	
+    
     // instance variables - replace the example below with your own
     private User user;
     private ArrayList<String> accoutInfo;
-    private ArrayList<Product> productList;
+    public static ArrayList<Product> productList;
     /**
      * Constructor for objects of class UserInterface
      */
     
     public UserInterface()
     {
-    	FileOperation.createFile("AccountInfo.txt");
-    	FileOperation.createFile("ProductInfo.txt");
-    	FileOperation.initialAccountFile();
+        FileOperation.createFile("AccountInfo.txt");
+        FileOperation.createFile("ProductInfo.txt");
+        FileOperation.initialAccountFile();
         // initialise instance variables
         user = new User();
         accoutInfo = new ArrayList<String>();
         productList = new ArrayList<Product>();
     }
     
-    
     public void main() {
-    	initialInterface();
-    	
+        initialInterface();
+        menu();
+        for(Product product:productList)
+        {
+            product.displayProductInfo();
+        }
     }
-    
     
     public void initialInterface()
     {
@@ -61,99 +65,141 @@ public class UserInterface
                 inputResonable = true;
             }
             else {
-            	System.out.println("You can only input 1 or 2!");
+                System.out.println("You can only input 1 or 2!");
             }
         }
         switch (option) {
-		case 1:
-			loginInterface();
-			break;
-		case 2:
-			Customer customer = new Customer();
-//			Customer customer = new Customer(customerID, customerName, customerAuthority, customerPassword)
-			break;
-		default:
-			break;
-		}
+        case 1:
+            loginInterface();
+            break;
+        case 2:
+            Customer customer = new Customer();
+//          Customer customer = new Customer(customerID, customerName, customerAuthority, customerPassword)
+            break;
+        default:
+            break;
+        }
     }
     
     public void loginInterface()
     {
-    	String userName;
-    	String passWord;
-    	boolean flag = false;
-    	Scanner input = new Scanner(System.in);
-    	System.out.println("This is login page!");
-    	while(!flag)
-    	{
-    		System.out.println("Please input your username:");
-    		userName = input.nextLine();
-    		System.out.println("Please input your password");
-    		passWord = input.nextLine();
-    		if(testLogin(userName,passWord))
-    		{
-    			flag = true;
-    		}
-    		else {
-    			System.out.println("your account information is wrong!");
-    		}
-    	}
-		showShelfInterface();
+        String userName;
+        String passWord;
+        boolean flag = false;
+        Scanner input = new Scanner(System.in);
+        System.out.println("This is login page!");
+        while(!flag)
+        {
+            System.out.println("Please input your username:");
+            userName = input.nextLine();
+            System.out.println("Please input your password");
+            passWord = input.nextLine();
+            if(testLogin(userName,passWord))
+            {
+                flag = true;
+            }
+            else {
+                System.out.println("your account information is wrong!");
+            }
+        }
+        showShelfInterface();
     }
     
     public boolean testLogin(String uName,String uPassword)
     {
-    	String AccountInfo = uName +"," + uPassword;
-    	String item;
-    	boolean find = false;
-    	File file = new File("./AccountInfo.txt");
-    	try {
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			while((item = br.readLine()) != null)
-			{
-				if (item.equals(AccountInfo))
-				{
-					find = true;
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-    	return find;
+        String AccountInfo = uName +"," + uPassword;
+        String item;
+        boolean find = false;
+        File file = new File("./AccountInfo.txt");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while((item = br.readLine()) != null)
+            {
+                if (item.equals(AccountInfo))
+                {
+                    find = true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return find;
     }
+    
     
     public void showShelfInterface()
     {
-    	readProduct();
-    	displayProduct();
+        
+        readProduct();
+        displayProduct();
     }
     
     public void readProduct()
     {
-    	String productItem;
-    	String[] productInfo = new String[7];
-    	File file = new File("./ProductInfo.txt");
-    	try {
-			BufferedReader brr = new BufferedReader(new FileReader(file));
-			while((productItem = brr.readLine()) != null)
-			{
-				
-				productInfo = productItem.split(",");
-				Product product = new Product(Integer.parseInt(productInfo[0]),productInfo[1],productInfo[2],Integer.parseInt(productInfo[3]),productInfo[4],productInfo[5],Double.parseDouble(productInfo[6]));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        String productItem;
+        String[] productInfo = new String[7];
+        File file = new File("./ProductInfo.txt");
+        try {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        System.out.println("hahaha");
+        while((productItem = br.readLine()) != null)
+        {
+                
+            productInfo = productItem.split(",");
+            Product product = new Product(Integer.parseInt(productInfo[0]),productInfo[1],productInfo[2],Integer.parseInt(productInfo[3]),productInfo[4],productInfo[5],Double.parseDouble(productInfo[6]));
+            productList.add(product);
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
     }
     
     public void displayProduct()
     {
         System.out.println("----------ProductList--------");
-    	for (Product product:productList)
-    	{
-    		product.displayProductInfo();
-    	}
+        for (Product product:productList)
+        {
+            product.displayProductInfo();
+        }
     }
     
-
+    public void menu()
+    {
+        boolean valid = false;
+        while(!valid) {
+            System.out.println("Please input your options:");
+            System.out.println("Press 1 to add product");
+            System.out.println("Press 2 to delete product");
+            System.out.println("Press 3 to edit product");
+            System.out.println("Press 4 to log out");
+            Scanner input = new Scanner(System.in);
+            int option = input.nextInt();
+            if (option >= 1 && option <= 4)
+            {
+                valid = true;
+            }
+            else {
+                System.out.println("You can only input 1 to 4!");
+            }
+        
+            switch (option) {
+            case 1:
+                ManageProductController.addProduct();
+                break;
+            case 2:
+                ManageProductController.deleteProduct();
+                break;
+            case 3:
+                ManageProductController.editProduct();
+                break;
+            case 4:
+                AccountController.logOut();
+                break;
+            default:
+                break;
+                
+            }
+        }
+        
+    }
 }
